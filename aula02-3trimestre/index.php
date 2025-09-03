220a99d533a1bd01ced956d22bc73a2c99e1ea47
vivyan <vivyan.rmartins@aluno.edu.es.gov.br>
2025-09-03 11:05:30 -0300

    Create index.php

---
+++ aula02-3trimestre/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Aula 02-php</title>
+    </head>
+    <body>
+        <?php
+        $nome1="Thainara";
+        $nome2="vivyan";
+
+        echo "meu nome é" .$nome1 ."e minha dupla é" .$nome2;
+        ?>
+    </body>
+</html>
