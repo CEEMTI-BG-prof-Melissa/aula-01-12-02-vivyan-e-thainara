b3369f1ea7093487c41b93699c01a7da8df16670
vivyan <vivyan.rmartins@aluno.edu.es.gov.br>
2025-09-03 10:41:06 -0300

    Create index.php

---
+++ aula01-3trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Aula 01 PHP </title>
+    </head>
+    <body>
+        <?php
+            echo "Olá, mundo!";
+            echo "thainara e vivyan";
+        ?>
+    </body>  
+</html>  
