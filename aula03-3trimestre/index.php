b1742850120f8435c23383508b4afd004a1b01ea
vivyan <vivyan.rmartins@aluno.edu.es.gov.br>
2025-09-10 10:59:45 -0300

    Update index.php

--- aula03-3trimestre/index.php
+++ aula03-3trimestre/index.php
@@ -14,7 +14,9 @@
         echo "meu nome é" .$nome1 ."e minha dupla é" .$nome2;
         echo "meu nome é" .$nome1 ."e minha dupla é" .$nome2;
 
-        echo "<br> <br> a soma de $n1 com $n2 é igual a soma $soma."
+        echo "<br> <br> a soma de $n1 com $n2 é igual a soma $soma.";
+
+        echo "<br><br> o resultado da subtração de $n1 e $n2 é igual a: " .($n1-$n2) .".";
         ?>
     </body>
 </html>
