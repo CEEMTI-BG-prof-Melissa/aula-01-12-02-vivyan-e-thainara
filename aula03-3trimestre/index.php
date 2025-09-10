b42bdfa87f8d87db56af070a3d3e9716f9ef3b81
vivyan <vivyan.rmartins@aluno.edu.es.gov.br>
2025-09-10 10:54:17 -0300

    Create index.php

---
+++ aula03-3trimestre/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Aula 03-php</title>
+    </head>
+    <body>
+        <?php
+        $nome1="Thainara";
+        $nome2="vivyan";
+
+        $n1= 10
+        $n2=2;
+        $soma=$n1 + $n2;
+
+        echo "meu nome é" .$nome1 ."e minha dupla é" .$nome2;
+        echo "meu nome é" .$nome1 ."e minha dupla é" .$nome2;
+
+        echo "<br> <br> a soma de $n1 com $n2 é igual a soma $soma."
+        ?>
+    </body>
+</html>
