199a2ee37347187277937b6d2b0c3344a19eebf8
vivyan <vivyan.rmartins@aluno.edu.es.gov.br>
2025-09-24 09:09:43 -0300

    Create index.php

---
+++ aula5-3trimestre/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Aula 05-PHP</title>
+    </head>
+
+    <body>
+        <?php
+            $n1= 2;
+            $n2= $_GET["x"];
+
+            echo "Os valores informados são $n1 e $n2 <br>";
+            echo "O valor absoluto de $n2 é " .abs($n2);
+            echo "<br> o resultado de $n2 <sup>$n1</sup> é igual a " .pow($n2,$n1);
+            echo "A raiz quadrada de $n2 é " .sqrt($n2);
+            echo "<br> O número $n2 arredondado é" .round($n2);
+            echo "<br> O número de $n2 arredondado para cima ".cell($n2);
+            echo "<br> O número"
+        ?>
+       
+    </body>
+</html>
