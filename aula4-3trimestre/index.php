7f9eb3dd54b7c41b12129ad88b34c59be1e7bb4a
vivyan <vivyan.rmartins@aluno.edu.es.gov.br>
2025-09-30 10:51:49 -0300

    Update index.php

--- aula4-3trimestre/index.php
+++ aula4-3trimestre/index.php
@@ -8,14 +8,18 @@
             $n1= 2;
             $n2= $_GET["x"];
 
-            echo "Os valores informados são $n1 e $n2 <br>";
-            echo "O valor absoluto de $n2 é " .abs($n2);
+            echo "<br> Os valores informados são $n1 e $n2 <br>";
+            echo "<br> O valor absoluto de $n2 é " .abs($n2);
             echo "<br> o resultado de $n2 <sup>$n1</sup> é igual a " .pow($n2,$n1);
-            echo "A raiz quadrada de $n2 é " .sqrt($n2);
+            echo "<br> A raiz quadrada de $n2 é " .sqrt($n2);
             echo "<br> O número $n2 arredondado é" .round($n2);
-            echo "<br> O número de $n2 arredondado para cima ".cell($n2);
-            echo "<br> O número de $n2 arredonda para baixo ".flor($n2);
+            echo "<br> O número de $n2 arredondado para cima é igua a ".cell($n2);
+            echo "<br> O número de $n2 arredonda para baixo é igual a ".flor($n2);
+            echo "<br> A parte inteira de $n2 é igual a " .intval($n2);
+            echo "<br> um pirulito custa R$"  .number_format($n1, 2);
+            echo "<br>";
         ?>
+
        
     </body>
 </html>
