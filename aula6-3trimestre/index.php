2e6d4f882401da731483c82609cb72fa02cbe166
vivyan <vivyan.rmartins@aluno.edu.es.gov.br>
2025-09-30 11:09:57 -0300

    Create index.php

---
+++ aula6-3trimestre/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Aula 06 - operadores de atribuição</title>
+    </head>
+    <body>
+        <?php
+            <h3>Operadores de Atribuição</h3>
+            <hr>
+
+            <?php
+                $a=2;
+                $b=$a+5;
+
+                echo"A é igual a $a e é igual a $b.";
+                echo"<br><br>";
+
+                $a=$a+3
+                echo"Agora A é igual á $a.";
+                echo"<br><br>";
+
+                $b+=8;
+                echo"A agora B é igual a $b.";
+                echoi"<br><br>";
+
+                $b-=5;
+                echo"Agora B é igual a $b.";
+        ?>
+
+       
+    </body>
+</html>
