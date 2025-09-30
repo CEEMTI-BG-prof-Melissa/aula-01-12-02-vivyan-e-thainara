fb4455091bfe5130d23ea4bb1528dde66b14e96e
vivyan <vivyan.rmartins@aluno.edu.es.gov.br>
2025-09-30 10:32:47 -0300

    Update index.php

--- aula4-3trimestre/index.php
+++ aula4-3trimestre/index.php
@@ -1,17 +1,21 @@
 <html>
     <head>
-        <title>Aula 04-PHP</title>
+        <title>Aula 05-PHP</title>
     </head>
+
     <body>
         <?php
-            $n1= $_GET["a"];
-            $n2= $_GET["b"];
+            $n1= 2;
+            $n2= $_GET["x"];
 
-            echo "A soma de $n1 com $n2 é igual a " .($n1+$n2);
-            echo "A subtração de $n1 com $n2 é igual a " .($n1-$n1);
-            echo "A multiplicação de $n1 com $n2 é igual a " .($n1*$n2);
-            echo "A divisão de $n1 com $n2 é igual a " .($n1/$n2);
-            echo "O resto de $n1 com $n2 é igual a " .($n1%$n2);
+            echo "Os valores informados são $n1 e $n2 <br>";
+            echo "O valor absoluto de $n2 é " .abs($n2);
+            echo "<br> o resultado de $n2 <sup>$n1</sup> é igual a " .pow($n2,$n1);
+            echo "A raiz quadrada de $n2 é " .sqrt($n2);
+            echo "<br> O número $n2 arredondado é" .round($n2);
+            echo "<br> O número de $n2 arredondado para cima ".cell($n2);
+            echo "<br> O número de $n2 arredonda para baixo ".flor($n2);
         ?>
+       
     </body>
 </html>
